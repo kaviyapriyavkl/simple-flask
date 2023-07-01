bbdeb8da2c56b65a1a446c6568d018268061e2c6
Kaviyapriya V K <119722892+kaviyapriyavkl@users.noreply.github.com>
2023-07-01 12:00:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request
+from pymongo import MongoClient
+from flask import jsonify
+
+app = Flask(__name__)
+
+# root route
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+# Set up MongoDB connection and collection
+client = MongoClient('mongodb://localhost:27017/')
+
+# Create database named demo if it doesn't exist already
+db = client['demo']
+
+# Create collection named data if it doesn't exist already
+collection = db['data']
+collection2 = db['data2']
+# Add data to MongoDB route
+@app.route('/add_data', methods=['POST'])
+def add_data():
+	# Get data from request
+	fromdata = request.json
+	
+
+	# Insert data into MongoDB
+	collection.insert_one(fromdata)
+	collection2.insert_one(fromdata)
+	return 'Data added to MongoDB'
+
+# Another API route
+@app.route('/get_data', methods=['GET'])
+def get_data():
+	# Retrieve data from MongoDB
+	data = collection.find()
+
+	# Prepare a response
+	response = []
+	for item in data:
+		if '_id' in item:
+			item['_id'] = str(item['_id'])
+		response.append(item)
+	print(response)
+
+	return response
+
+# Yet another API route
+@app.route('/delete_data', methods=['DELETE'])
+def delete_data():
+	# Delete all data from MongoDB collection
+	result = collection.delete_many({})
+
+	return f'Deleted {result.deleted_count} documents'
+
+if __name__ == '__main__':
+	app.run()
+
+
+'''from flask import Flask, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# root route
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+# Set up MongoDB connection and collection
+client = MongoClient('mongodb://localhost:27017/')
+
+# Create database named demo if they don't exist already
+db = client['demo']
+
+# Create collection named data if it doesn't exist already
+collection = db['data']
+#collection = db['name']
+
+# Add data to MongoDB route
+@app.route('/add_data', methods=['POST'])
+def add_data():
+	# Get data from request
+	data = request.json
+
+	# Insert data into MongoDB
+	collection.insert_one(data)
+    
+	return 'Data is added to MongoDB'
+
+
+if __name__ == '__main__':
+	app.run()   
+    
+	'''
